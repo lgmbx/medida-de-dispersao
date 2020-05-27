e78c66ff43d7a8e4136867e94559c0b54477b4ac
lgmbx <gustavo_x@live.com>
2020-05-27 09:48:20 -0300

    segunda versao

--- MedidasDeDispersao/MedidasDeDispersao/Form1.Designer.cs
+++ MedidasDeDispersao/MedidasDeDispersao/Form1.Designer.cs
@@ -29,27 +29,33 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.lblAmplitude = new System.Windows.Forms.Label();
             this.lblVariancia = new System.Windows.Forms.Label();
             this.lblDesvio = new System.Windows.Forms.Label();
             this.lblCoeficiente = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.Limpar = new System.Windows.Forms.Button();
+            this.lblAmplitude = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // TextBox
             // 
-            this.TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.TextBox.Location = new System.Drawing.Point(12, 12);
+            this.TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.TextBox.Location = new System.Drawing.Point(17, 17);
             this.TextBox.Multiline = true;
             this.TextBox.Name = "TextBox";
-            this.TextBox.Size = new System.Drawing.Size(306, 82);
+            this.TextBox.Size = new System.Drawing.Size(403, 92);
             this.TextBox.TabIndex = 0;
             // 
             // Calcular
             // 
-            this.Calcular.Location = new System.Drawing.Point(324, 12);
+            this.Calcular.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightBlue;
+            this.Calcular.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SteelBlue;
+            this.Calcular.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSkyBlue;
+            this.Calcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Calcular.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calcular.Location = new System.Drawing.Point(17, 119);
             this.Calcular.Name = "Calcular";
-            this.Calcular.Size = new System.Drawing.Size(71, 46);
+            this.Calcular.Size = new System.Drawing.Size(71, 30);
             this.Calcular.TabIndex = 1;
             this.Calcular.Text = "Calcular";
             this.Calcular.UseVisualStyleBackColor = true;
@@ -58,74 +64,78 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 109);
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 167);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.Size = new System.Drawing.Size(76, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Amplitude:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 140);
+            this.label2.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 196);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.Size = new System.Drawing.Size(68, 17);
             this.label2.TabIndex = 3;
             this.label2.Text = "Variância:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(230, 109);
+            this.label3.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 225);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.Size = new System.Drawing.Size(101, 17);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Desvio padrão:";
+            this.label3.Text = "Desvio padrao:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(230, 140);
+            this.label4.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 254);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.Size = new System.Drawing.Size(80, 17);
             this.label4.TabIndex = 5;
             this.label4.Text = "Coeficiente:";
-            // 
-            // lblAmplitude
-            // 
-            this.lblAmplitude.AutoSize = true;
-            this.lblAmplitude.Location = new System.Drawing.Point(68, 109);
-            this.lblAmplitude.Name = "lblAmplitude";
-            this.lblAmplitude.Size = new System.Drawing.Size(0, 13);
-            this.lblAmplitude.TabIndex = 6;
             // 
             // lblVariancia
             // 
             this.lblVariancia.AutoSize = true;
-            this.lblVariancia.Location = new System.Drawing.Point(68, 140);
+            this.lblVariancia.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVariancia.Location = new System.Drawing.Point(83, 196);
             this.lblVariancia.Name = "lblVariancia";
-            this.lblVariancia.Size = new System.Drawing.Size(0, 13);
+            this.lblVariancia.Size = new System.Drawing.Size(0, 17);
             this.lblVariancia.TabIndex = 7;
             // 
             // lblDesvio
             // 
             this.lblDesvio.AutoSize = true;
-            this.lblDesvio.Location = new System.Drawing.Point(315, 109);
+            this.lblDesvio.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesvio.Location = new System.Drawing.Point(116, 225);
             this.lblDesvio.Name = "lblDesvio";
-            this.lblDesvio.Size = new System.Drawing.Size(0, 13);
+            this.lblDesvio.Size = new System.Drawing.Size(0, 17);
             this.lblDesvio.TabIndex = 8;
             // 
             // lblCoeficiente
             // 
             this.lblCoeficiente.AutoSize = true;
-            this.lblCoeficiente.Location = new System.Drawing.Point(299, 140);
+            this.lblCoeficiente.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCoeficiente.Location = new System.Drawing.Point(95, 254);
             this.lblCoeficiente.Name = "lblCoeficiente";
-            this.lblCoeficiente.Size = new System.Drawing.Size(0, 13);
+            this.lblCoeficiente.Size = new System.Drawing.Size(0, 17);
             this.lblCoeficiente.TabIndex = 9;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(324, 64);
+            this.button2.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightBlue;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SteelBlue;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSkyBlue;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(349, 119);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(71, 30);
             this.button2.TabIndex = 11;
@@ -133,24 +143,48 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // Limpar
+            // 
+            this.Limpar.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightBlue;
+            this.Limpar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SteelBlue;
+            this.Limpar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSkyBlue;
+            this.Limpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Limpar.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Limpar.Location = new System.Drawing.Point(98, 119);
+            this.Limpar.Name = "Limpar";
+            this.Limpar.Size = new System.Drawing.Size(71, 30);
+            this.Limpar.TabIndex = 12;
+            this.Limpar.Text = "Limpar";
+            this.Limpar.UseVisualStyleBackColor = true;
+            this.Limpar.Click += new System.EventHandler(this.Limpar_Click);
+            // 
+            // lblAmplitude
+            // 
+            this.lblAmplitude.AutoSize = true;
+            this.lblAmplitude.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmplitude.Location = new System.Drawing.Point(91, 167);
+            this.lblAmplitude.Name = "lblAmplitude";
+            this.lblAmplitude.Size = new System.Drawing.Size(0, 17);
+            this.lblAmplitude.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 171);
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(438, 285);
+            this.Controls.Add(this.lblAmplitude);
+            this.Controls.Add(this.Limpar);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.lblCoeficiente);
             this.Controls.Add(this.lblDesvio);
             this.Controls.Add(this.lblVariancia);
-            this.Controls.Add(this.lblAmplitude);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Calcular);
             this.Controls.Add(this.TextBox);
-            this.MaximumSize = new System.Drawing.Size(420, 210);
-            this.MinimumSize = new System.Drawing.Size(420, 210);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Medida de dispersão";
@@ -167,11 +201,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label lblAmplitude;
         private System.Windows.Forms.Label lblVariancia;
         private System.Windows.Forms.Label lblDesvio;
         private System.Windows.Forms.Label lblCoeficiente;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Limpar;
+        private System.Windows.Forms.Label lblAmplitude;
     }
 }
 
--- MedidasDeDispersao/MedidasDeDispersao/Form1.cs
+++ MedidasDeDispersao/MedidasDeDispersao/Form1.cs
@@ -19,58 +19,65 @@ namespace MedidasDeDispersao {
         bool calc = false;
 
         private void Calcular_Click(object sender, EventArgs e) {
+            try {
+                if (calc == false) {
 
-            if (calc == false) {
+                    string aux = TextBox.Text.Trim();
+                    string[] txt = aux.Split(' ');
 
-                string aux = TextBox.Text.Trim();
-                string[] txt = aux.Split(' ');
+                    foreach (string x in txt) {
+                        numeros.Add(double.Parse(x));
+                    }
 
-                foreach (string x in txt) {
-                    numeros.Add(double.Parse(x));
+                    numeros.Sort();
+
+                    StringBuilder sb = new StringBuilder();
+                    foreach (double x in numeros) {
+                        sb.Append(x.ToString() + " ");
+                    }
+                    TextBox.Text = sb.ToString();
+
+                    lblAmplitude.Text = Amplitude(numeros);
+
+                    lblVariancia.Text = Variancia(numeros);
+
+                    lblDesvio.Text = Desvio(lblVariancia);
+
+                    lblCoeficiente.Text = Coeficiente(lblDesvio, numeros);
+                    calc = true;
+                }
+                else {
+                    numeros.Clear();
+                    calc = false;
                 }
 
-                numeros.Sort();
-
-                StringBuilder sb = new StringBuilder();
-                foreach (double x in numeros) {
-                    sb.Append(x.ToString() + " ");
-                }
-                TextBox.Text = sb.ToString();
-
-                lblAmplitude.Text = Amplitude(numeros);
-
-                lblVariancia.Text = Variancia(numeros);
-
-                lblDesvio.Text = Desvio(lblVariancia);
-
-                lblCoeficiente.Text = Coeficiente(lblDesvio, numeros);
-                calc = true;
             }
-            else {
-                numeros.Clear();
-                calc = false;
+            catch (Exception er) {
+
+                MessageBox.Show(er.Message, "ERRO");
             }
+
         }
 
         private string Amplitude(List<double> num) {
             double max = num.Max();
             double min = num.Min();
             double res = max - min;
-            return res.ToString() ;
+            return res.ToString();
         }
-        
+
         private string Variancia(List<double> num) {
-            
+
             double soma = num.Sum();
             double somaQ = num.Sum(x => x * x);
 
             double qtd = num.Count();
 
             double res = (somaQ - ((soma * soma) / qtd)) / (qtd - 1);
-            
+
             return res.ToString("F4");
         }
-    
+
         private string Desvio(Label lbl) {
 
             double res = Math.Sqrt(double.Parse(lbl.Text));
@@ -86,14 +93,28 @@ namespace MedidasDeDispersao {
             double res = (desvio / media) * 100;
 
             return res.ToString("F2") + "%";
-        
+
         }
 
         private void button2_Click(object sender, EventArgs e) {
-            MessageBox.Show("As amostras devem ser separadas por apenas um espaçamento\n" +
+            MessageBox.Show("Os numeros devem ser separados por apenas um espaçamento\n" +
                 "Apenas numeros e virgula devem ser digitados no campo\n" +
-                "Ex: 1,3 3 2 6 7,6\n" +
-                "Os numeros da amostra serão organizados em ROL automanticamente", "Instruções");
+                "Os numeros da amostra serão organizados em ROL automaticamente" +
+                "entrada: 1,3 2,4 2 9 7,6\n" +
+                "saida: 1,3 2 2,4 7,6 9\n" +
+                "\nCaso haja necessidade de adicionar um numero após o calculo ja ter sido efetuado, "+
+                "inseri-lo no final da serie de dados e clicar duas vezes em calcular", "Instruções");
+        }
+
+
+        private void Limpar_Click(object sender, EventArgs e) {
+            numeros.Clear();
+            TextBox.Text = "";
+            lblAmplitude.ResetText();
+            lblCoeficiente.ResetText();
+            lblDesvio.ResetText();
+            lblVariancia.ResetText();
+            calc = false;
         }
     }
 }
