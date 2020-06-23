969f64aab7c972e555daecde0aedfb27d14a2206
lgmbx <gustavo_x@live.com>
2020-06-23 13:57:41 -0300

    icone e entrega do projeto

--- MedidasDeDispersao/MedidasDeDispersao/Form1.Designer.cs
+++ MedidasDeDispersao/MedidasDeDispersao/Form1.Designer.cs
@@ -23,6 +23,7 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.TextBox = new System.Windows.Forms.TextBox();
             this.Calcular = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -185,6 +186,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Calcular);
             this.Controls.Add(this.TextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(454, 324);
             this.MinimumSize = new System.Drawing.Size(454, 324);
             this.Name = "Form1";
